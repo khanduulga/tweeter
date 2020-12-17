5d2ff1adb09a30206a74a64f8972ad83e35ebd4c
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-17 02:50:58 +0000

    Removed unnecessary styling on css files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,3 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function(event) {
-  const article = $("#all-tweets article");
-  console.log(article);
-});
