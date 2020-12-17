04eff8506d2ad2807b7066ff4873685fd243a57b
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-17 01:19:57 +0000

    Dynamic counter: pulled out counter element from event listeners using jquery and made small styling changes to change to red when counter hits negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+//javascipt for counter under the Compose message box
+
+$(document).ready(function() {
+  console.log("HI!")
+  const textbox = $("#tweet-text");
+  textbox.on("keyup", function(event) {
+    const charLength = $(this).val().length;
+    let counter = $($(this).next().children()[1]);
+    if ((140 - charLength) < 0) {
+      counter.addClass("red-counter");
+    } else {
+      counter.removeClass("red-counter");
+    }
+    counter.val(140 - charLength);
+  })
+  
+})
+
+
+
+//default_value
