76c8035462ff0441cfdb569ae4abc92bcdc23786
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-18 05:30:57 +0000

    Getting ready to submit: cleaned up code and added missing functionality (user avatar, text-area length counter)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,54 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */ 
 
+//helper for converting dates to strings
+ //TAKEN FROM STACKOVERFLOW BY Masih Jahangiri
+function timeSince(date) {
+
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+
 //creates and returns a string using given tweet object
 function createTweetElement(tweet) {
   const $tweet = `
   <article class="tweet">
-  <header>
-  <h3>${escape(tweet.user.name)}</h3>
-  <h3 class="handle">${escape(tweet.user.handle)}</h3>
-  </header>  
-  <p>${escape(tweet.content.text)}</p>
-  <footer>
-  <div>${escape(tweet.created_at)}</div>
-  <div>Share and Subscribe</div>
-  </footer>  
+    <header>
+      <div>
+        <img src="${tweet.user.avatars}" class="avatar">
+        <h4>${escape(tweet.user.name)}</h4>
+      </div>
+      <div>
+        <h4 class="handle">${escape(tweet.user.handle)}</h4>
+      </div>
+    </header>  
+      <p>${escape(tweet.content.text)}</p>
+    <footer>
+      <div>${escape(timeSince(tweet.created_at))}</div>
+      <div>F R L</div>
+    </footer>  
   </article>  
   `;  
   return $tweet;
@@ -43,11 +78,10 @@ function clearTweets() {
 function submitTweet(event) {
   event.preventDefault();
   const tweet = $(".new-tweet-box").serialize();
-  const tweetForLengthCheck = tweet.replaceAll("%20", "+");
-  const tweetLength = tweetForLengthCheck.length - 5;           //for "text="
+  const tweetLength = decodeURIComponent(tweet).length - 5;           //for "text="
 
   //check if tweet is empty or too lengthy
-  if (tweetLength < 141 && tweetForLengthCheck.length !== 5) {
+  if (tweetLength < 141 && tweetLength !== 0) {
     //animate away error message
     $(".new-tweet #error").slideUp("slow");
 
@@ -56,6 +90,8 @@ function submitTweet(event) {
     .then((res) => {
       clearTweets();
       loadTweets();
+      $(".new-tweet #tweet-text").val("");
+      $(".new-tweet output").val(140);
     })
   } else if (tweetLength > 140) {
     //For being too long
